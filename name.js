181807bb9df7ac7e1fc9325e9971baf566b0f2ba
abidmehmod <160239077+abidmehmod@users.noreply.github.com>
2024-03-23 19:35:17 +0500

    all print names

---
+++ name.js
@@ -0,0 +1,5 @@
+"use strict";
+let members = ["Abid", "khan", "imran", "salman"];
+for (let i = 0; i < members.length; i++) {
+    console.log(members[i]);
+}
---
+++ name.ts
@@ -0,0 +1,6 @@
+let members : string [] = ["Abid","khan","imran","salman"];
+for(let i = 0; i<members.length; i++){
+
+    console.log(members[i]);
+
+}
